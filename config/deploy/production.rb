969354cefd76f9cfca78e295cc9b88a63d1f3844
Masaaki-Kimura06 <kimu06.0412@gmail>
2020-04-17 12:48:22 +0900

    aa

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'http://18.180.110.137/', user: 'ec2-user', roles: %w{app db web}
+server '18.180.110.137', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
